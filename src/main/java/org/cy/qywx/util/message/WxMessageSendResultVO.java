04576a8ec96f30cff99f1552864af4a12ebede4f
CY <you@example.com>
2026-06-03 22:52:51 +0800

    refactor: add serialVersionUID to WxMessageSendResultVO

--- src/main/java/org/cy/qywx/util/message/WxMessageSendResultVO.java
+++ src/main/java/org/cy/qywx/util/message/WxMessageSendResultVO.java
@@ -13,6 +13,9 @@ import java.io.Serializable;
 @Data
 public class WxMessageSendResultVO implements Serializable {
 
+    /** 序列化版本号 */
+    private static final long serialVersionUID = 1L;
+
     /**
      * 字段说明：消息 ID。
      *
